78d5c926ad9bfad7362872c063b1e8866743b4cd
shaqsnake <shaqsnake@gmail.com>
2019-08-21 16:41:52 +0800

    Issue #2.
    * Updated encode process to support encode non-ascii characters.
    * Add unittest to test enconding chinese character.
    
    NOTE:
    * Need more unittests.

--- src/Utf8.cpp
+++ src/Utf8.cpp
@@ -3,12 +3,24 @@
  * @Author: shaqsnake
  * @Email: shaqsnake@gmail.com
  * @Date: 2019-08-20 14:41:29
- * @LastEditTime: 2019-08-21 14:30:27
+ * @LastEditTime: 2019-08-21 16:31:24
  * @Description: An implementation of class utf8::Utf8.
  */
 
 #include <string>
 #include <utf8/Utf8.hpp>
+
+namespace {
+    size_t calulateBits(utf8::UnicodeUnit codePoint) {
+        size_t bits = 0;
+        while (codePoint) {
+            bits++;
+            codePoint = codePoint >> 1;
+        }
+
+        return bits;
+    }
+}
 
 namespace utf8 {
 /**
@@ -34,7 +46,22 @@ std::vector<UnicodeUnit> asciiToUnicode(const std::string &asciis) {
 std::vector<Utf8Unit> Utf8::encode(const std::vector<UnicodeUnit> &unicodes) {
     std::vector<Utf8Unit> res;
     for (auto const unicode : unicodes) {
-        res.push_back(unicode);
+        const auto bits = calulateBits(unicode);
+        if (bits <= 7) {
+            res.push_back(static_cast<Utf8Unit>(unicode & 0x7F));
+        } else if (bits <= 11) {
+            res.push_back(static_cast<Utf8Unit>(unicode >> 6 & 0x1F) + 0xC0);
+            res.push_back(static_cast<Utf8Unit>(unicode & 0x3F) + 0x80);
+        } else if (bits <= 16) {
+            res.push_back(static_cast<Utf8Unit>(unicode >> 12 & 0x0F) + 0xE0);
+            res.push_back(static_cast<Utf8Unit>(unicode >> 6 & 0x3F) + 0x80);
+            res.push_back(static_cast<Utf8Unit>(unicode & 0x3F) + 0x80);
+        } else {
+            res.push_back(static_cast<Utf8Unit>(unicode >> 18 & 0x07)  + 0xF0);
+            res.push_back(static_cast<Utf8Unit>(unicode >> 12 & 0x3F)  + 0x80);
+            res.push_back(static_cast<Utf8Unit>(unicode >> 6 & 0x3F) + 0x80);
+            res.push_back(static_cast<Utf8Unit>(unicode & 0x3F) + 0x80);
+        }
     }
 
     return res;
--- test/src/Utf8Tests.cpp
+++ test/src/Utf8Tests.cpp
@@ -3,7 +3,7 @@
  * @Author: shaqsnake
  * @Email: shaqsnake@gmail.com
  * @Date: 2019-08-20 14:48:52
- * @LastEditTime: 2019-08-21 14:30:29
+ * @LastEditTime: 2019-08-21 16:32:10
  * @Description: Unittests of class utf8::Utf8.
  */
 
@@ -19,6 +19,13 @@ TEST(Utf8Tests, ParseAsciiToUnicode) {
 TEST(Utf8Tests, EncodeAscii) {
     const std::vector<utf8::Utf8Unit> expected = {0x55, 0x74, 0x66, 0x2D, 0x38};
     utf8::Utf8 utf8;
-    auto actual = utf8.encode(utf8::asciiToUnicode("Utf-8"));
+    const auto actual = utf8.encode(utf8::asciiToUnicode("Utf-8"));
+    ASSERT_EQ(expected, actual);
+}
+
+TEST(Utf8Tests, EncodeChinese) {
+    const std::vector<utf8::Utf8Unit> expected{0xF0, 0xA3, 0x8E, 0xB4};
+    utf8::Utf8 utf8;
+    const auto actual = utf8.encode({0x233B4});
     ASSERT_EQ(expected, actual);
 }
